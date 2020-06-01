84cb6bd430ecfcb2c4e32127174d3c7fdafd218f
zouxf <zxf@t-chip.com.cn>
2020-06-01 11:15:24 +0800

    firefly: enable rk3288 overlay rootfs support
    Change-Id: I2910313892d97c889ea271e3baf92f5eaa4e639c

--- linux/Linux_Pack_Firmware/rockdev/rk3288-mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/rk3288-mkupdate.sh
@@ -14,9 +14,30 @@ if [ ! -f "package-file" ]; then
 	echo "Error:No found package-file!"
 	exit 1
 fi
+
+ALIGN()
+{
+	X=$1
+	A=$2
+	OUT=$(($((${X} + ${A} -1 ))&$((~$((${A}-1))))))
+	printf 0x%x ${OUT}
+}
+
+FSIZE=$(stat $(readlink -f Image/rootfs.img) -c %b)
+PSIZE=$(ALIGN $((${FSIZE}+204800)) 512)
+PARA_FILE=$(readlink -f Image/parameter.txt)
+
+ORIGIN=$(grep -Eo "0x[0-9a-fA-F]*@0x[0-9a-fA-F]*\(rootfs" $PARA_FILE)
+NEWSTR=$(echo $ORIGIN | sed "s/.*@/${PSIZE}@/g")
+OFFSET=$(echo $NEWSTR | grep -Eo "@0x[0-9a-fA-F]*" | cut -f 2 -d "@")
+NEXT_START=$(printf 0x%x $(($PSIZE + $OFFSET)))
+sed -i.orig "s/$ORIGIN/$NEWSTR/g" $PARA_FILE
+sed -i "/^CMDLINE.*/s/-@0x[0-9a-fA-F]*/-@$NEXT_START/g" $PARA_FILE
+
 ./afptool -pack ./ Image/update.img || pause
 ./rkImageMaker -RK320A Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
-echo "Making update.img OK."
+echo "Making ./Image/update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
+mv ${PARA_FILE}.orig ${PARA_FILE}
 exit $?
