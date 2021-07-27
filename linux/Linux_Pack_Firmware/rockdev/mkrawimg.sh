c1af35ae80ab9c31eb668a79dcbad8f125316747
liulq <llq@t-chip.com.cn>
2021-07-27 10:05:14 +0800

    firefly: mkrawimg.sh: auto modify rootfs size

--- linux/Linux_Pack_Firmware/rockdev/mkrawimg.sh
+++ linux/Linux_Pack_Firmware/rockdev/mkrawimg.sh
@@ -12,6 +12,41 @@ PARTITION_INDEX=0
 
 rm -rf ${RAWIMG}
 
+ALIGN()
+{
+	X=$1
+	A=$2
+	OUT=$(($((${X} + ${A} -1 ))&$((~$((${A}-1))))))
+	printf 0x%x ${OUT}
+}
+
+ROOTFS_LAST=$(grep "rootfs:grow" Image/parameter.txt)
+if [ -z "${ROOTFS_LAST}" ]
+then
+echo "Resize rootfs partition size"
+FILE_P=$(readlink -f Image/rootfs.img)
+FS_INFO=$(dumpe2fs -h ${FILE_P})
+BLOCK_COUNT=$(echo "${FS_INFO}" | grep "^Block count" | cut -d ":" -f 2 | tr -d "[:blank:]")
+INODE_COUNT=$(echo "${FS_INFO}" | grep "^Inode count" | cut -d ":" -f 2 | tr -d "[:blank:]")
+BLOCK_SIZE=$(echo "${FS_INFO}" | grep "^Block size" | cut -d ":" -f 2 | tr -d "[:blank:]")
+INODE_SIZE=$(echo "${FS_INFO}" | grep "^Inode size" | cut -d ":" -f 2 | tr -d "[:blank:]")
+BLOCK_SIZE_IN_S=$((${BLOCK_SIZE}>>9))
+INODE_SIZE_IN_S=$((${INODE_SIZE}>>9))
+SKIP_BLOCK=70
+EXTRA_SIZE=$(expr 50 \* 1024 \* 2 ) #50M
+
+FSIZE=$(expr ${BLOCK_COUNT} \* ${BLOCK_SIZE_IN_S} + ${INODE_COUNT} \* ${INODE_SIZE_IN_S} + ${EXTRA_SIZE} + ${SKIP_BLOCK})
+PSIZE=$(ALIGN $((${FSIZE})) 512)
+PARA_FILE=$(readlink -f Image/parameter.txt)
+
+ORIGIN=$(grep -Eo "0x[0-9a-fA-F]*@0x[0-9a-fA-F]*\(rootfs" $PARA_FILE)
+NEWSTR=$(echo $ORIGIN | sed "s/.*@/${PSIZE}@/g")
+OFFSET=$(echo $NEWSTR | grep -Eo "@0x[0-9a-fA-F]*" | cut -f 2 -d "@")
+NEXT_START=$(printf 0x%x $(($PSIZE + $OFFSET)))
+sed -i.orig "s/$ORIGIN/$NEWSTR/g" $PARA_FILE
+sed -i "/^CMDLINE.*/s/-@0x[0-9a-fA-F]*/-@$NEXT_START/g" $PARA_FILE
+fi
+
 for PARTITION in `cat ${PARAMETER} | grep '^CMDLINE' | sed 's/ //g' | sed 's/.*:\(0x.*[^)])\).*/\1/' | sed 's/,/ /g'`; do
         PARTITION_NAME=`echo ${PARTITION} | sed 's/\(.*\)(\(.*\))/\2/' | cut -f 1 -d ":"`
         PARTITION_START=`echo ${PARTITION} | sed 's/.*@\(.*\)(.*)/\1/'`
@@ -89,5 +124,13 @@ for PARTITION in ${PARTITIONS[@]}; do
 		fi
     fi
 done
+
+if [ -e ${PARA_FILE}.orig ]
+then
+	mv ${PARA_FILE}.orig ${PARA_FILE}
+	exit $?
+else
+	exit 0
+fi
 
 echo "mk raw img OK"
